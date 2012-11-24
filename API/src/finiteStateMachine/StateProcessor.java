f757b554b21a1bd7d820c1e965e1d95465e035df
Serhij Mulishov <serhij.mulishov@gmail.com>
2012-11-24 19:53:03 +0200

    Lesson 2. Step 0.1

--- API/src/finiteStateMachine/StateProcessor.java
+++
@@ -1,7 +0,0 @@
-package finiteStateMachine;
-
-public interface StateProcessor<Context, State> {
-
-    public boolean process(Context context, State state) throws StateMachineException;
-
-}
--- API/src/finiteStateMachine/StateRecognizer.java
+++
@@ -1,7 +0,0 @@
-package finiteStateMachine;
-
-public interface StateRecognizer<Context, State> {
-
-    public boolean recognize(Context context, State state) throws StateMachineException;
-
-}
