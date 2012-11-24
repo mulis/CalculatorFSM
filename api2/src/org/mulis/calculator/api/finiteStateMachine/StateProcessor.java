588b092a62ee65aee6afb246a49236e462402a24
Serhij Mulishov <serhij.mulishov@gmail.com>
2012-11-24 20:14:28 +0200

    Lesson 2. Step 0.1

--- api2/src/org/mulis/calculator/api/finiteStateMachine/StateProcessor.java
+++
@@ -1,7 +0,0 @@
-package org.mulis.calculator.api.finiteStateMachine;
-
-public interface StateProcessor<Context, State> {
-
-    public boolean process(Context context, State state) throws StateMachineException;
-
-}
--- api2/src/org/mulis/calculator/api/finiteStateMachine/StateRecognizer.java
+++
@@ -1,7 +0,0 @@
-package org.mulis.calculator.api.finiteStateMachine;
-
-public interface StateRecognizer<Context, State> {
-
-    public boolean recognize(Context context, State state) throws StateMachineException;
-
-}
